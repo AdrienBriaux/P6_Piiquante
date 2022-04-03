d991432f89af366784bfd4f9bc351814ca5369fc
AdrienBriaux <devaccess@hotmail.com>
2022-04-03 18:14:21 +0200

    work in progress

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -1,3 +1,4 @@
+
 const Sauce = require('../models/Sauce');
 const fs = require('fs');
 
@@ -7,7 +8,9 @@ exports.createThing = (req, res, next) => {
 
     const sauceObject = JSON.parse(req.body.sauce);
     delete sauceObject._id;
+
     const sauce = new Sauce({
+
         ...sauceObject,
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
@@ -21,6 +24,7 @@ exports.createThing = (req, res, next) => {
 exports.getAllThings = (req, res, next) => {
 
     Sauce.find()
+
         .then(sauces => res.status(200).json(sauces))
         .catch(error => res.status(400).json({ error }));
 };
@@ -29,15 +33,19 @@ exports.deleteThing = (req, res, next) => {
 
     Sauce.findOne({ _id: req.params.id })
 
-      .then(sauce => {
+        .then(sauce => {
 
-        const filename = sauce.imageUrl.split('/images/')[1];
-        fs.unlink(`images/${filename}`, () => {
-          Sauce.deleteOne({ _id: req.params.id })
-            .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
-            .catch(error => res.status(400).json({ error }));
-        });
-      })
+            const filename = sauce.imageUrl.split('/images/')[1];
 
-      .catch(error => res.status(500).json({ error }));
-  };
+            fs.unlink(`images/${filename}`, () => {
+
+                Sauce.deleteOne({ _id: req.params.id })
+
+                    .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
+                    .catch(error => res.status(400).json({ error }));
+            });
+        })
+
+        .catch(error => res.status(500).json({ error }));
+};
+
