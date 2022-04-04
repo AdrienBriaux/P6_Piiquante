922dd6b945e3cff2749b8bf5e5b7bd77e0c7afff
AdrienBriaux <devaccess@hotmail.com>
2022-04-04 11:03:25 +0200

    Importation multer et mise en place authentification sur toutes les routes

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const router = express.Router();
-
-const stuffCtrl = require('../controllers/stuff')
+const auth = require('../middleware/auth');
+const stuffCtrl = require('../controllers/stuff');
+const multer = require('../middleware/multer-config');
 
 // Route pour ajouter une nouvelle sauce
 
-router.post('/api/sauces', stuffCtrl.createThing);
+router.post('/api/sauces', auth, multer, stuffCtrl.createThing);
 
 // Route pour supprimer une sauce
 
-router.delete('/api/sauces/:id', stuffCtrl.deleteThing);
+router.delete('/api/sauces/:id', auth, stuffCtrl.deleteThing);
 
 // Route pour récupérer toutes les sauces
 
-router.get('/api/sauces', stuffCtrl.getAllThings);
+router.get('/api/sauces', auth, stuffCtrl.getAllThings);
 
 module.exports = router;
