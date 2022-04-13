c6754193e0fd2e3965f2dae2584e9daaa4fb183a
AdrienBriaux <devaccess@hotmail.com>
2022-04-13 16:34:38 +0200

    CORS Content-Security default-src

--- backend/app.js
+++ backend/app.js
@@ -20,8 +20,9 @@ app.use(helmet.noSniff());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization, Content-Security-Policy ');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization ');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Content-Security-Policy', "default-src 'self'");
     next();
 });
 
