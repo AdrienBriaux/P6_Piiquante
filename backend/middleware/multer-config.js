05bffc29e68f667a5eac939ebe414933a7e4efad
AdrienBriaux <devaccess@hotmail.com>
2022-04-19 10:31:51 +0200

    Ajout commentaires

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -7,19 +7,36 @@ const MIME_TYPES = {
     'image/png': 'png'
 };
 
+
+// Création de la sauvegarde des fichiers 
+
 const storage = multer.diskStorage({
+
+    // On défini où seront stocké les fichiers
 
     destination: (req, file, callback) => {
 
         callback(null, 'images')
     },
 
+    // Définition du nom des fichiers
+
     filename: (req, file, callback) => {
 
+        // On récupére le nom du fichier d'origine
+
         const name = file.originalname.split(' ').join('_');
+
+        // On créer l'extension du fichier
+
         const extension = MIME_TYPES[file.mimetype];
+
+        // Création du nouveau nom de fichier
+
         callback(null, name + Date.now() + '.' + extension);
     }
 })
+
+// Mise à disposition du middleware avec l'objet storage
 
 module.exports = multer({ storage }).single('image');
